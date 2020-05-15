299902c1c8a5433f4255456f371f5ee5709d371a
nagendra <=>
2020-05-15 12:47:22 +0530

    implemented create_list function

---
+++ linkedlist.c
@@ -0,0 +1,9 @@
+#include "linkedlist.h"
+List_ptr create_list(void)
+{
+  List_ptr list = malloc(sizeof(LinkedList));
+  list->first = NULL;
+  list->last = NULL;
+  list->length = 0;
+  return list;
+}
